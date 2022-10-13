8ee23d4fa3a4897ebd66651b09502669b975c0a9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 08:23:43 +0000

    Adding tarfile member sanitization to extractall()

--- test/lib/python3.8/site-packages/build/__main__.py
+++ test/lib/python3.8/site-packages/build/__main__.py
@@ -167,7 +167,26 @@ def build_package_via_sdist(srcdir, outdir, distributions, config_settings=None,
     sdist_out = tempfile.mkdtemp(prefix='build-via-sdist-')
     # extract sdist
     with tarfile.open(sdist) as t:
-        t.extractall(sdist_out)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t, sdist_out)
         try:
             builder = ProjectBuilder(os.path.join(sdist_out, sdist_name[: -len('.tar.gz')]))
             for distribution in distributions:
